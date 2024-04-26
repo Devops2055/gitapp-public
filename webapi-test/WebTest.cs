14891c2abb0d0e395f00ce1ca377cadd6aabea51
Fabio De Orazi <fabio.deorazi@gmail.com>
2024-04-26 07:24:54 +0200

    Folder refactoring to add test project in repository

---
+++ webapi-test/WebTest.cs
@@ -0,0 +1,13 @@
+namespace webapi_test;
+
+public class WebTest
+{
+    [Fact]
+    public void DemoTest()
+    {
+        int i = 1;
+        bool result = false;
+        if (i == 1) result = true;
+        Assert.True(result, "Value should be equal to 1");
+    }
+}
